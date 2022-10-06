19bc3fdf7e582abde00f68a728bc0fcbb0c74896
MateuszPostyou <mateusz.zoldak@postyou.de>
2022-10-06 11:04:14 +0200

    script.js update

--- src/Resources/public/js/script.js
+++ src/Resources/public/js/script.js
@@ -1,6 +1,4 @@
 let popup = document.querySelector('.form-popup');
 popup.querySelector('.success.fp-btn').addEventListener('click', () => {
     popup.classList.add('fp-saved');
-    document.html.classList.remove('noScroll');
-    document.body.classList.remove('noScroll');
 })
